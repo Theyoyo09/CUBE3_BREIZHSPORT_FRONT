1a7a528345ab665d494d0325d0b4359428c5d712
baptiste.nebout <baptiste16300@gmail.com>
2023-11-28 23:26:11 +0100

    ajout du component "à propos"

---
+++ src/app/apropos/apropos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-apropos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './apropos.component.html',
+  styleUrl: './apropos.component.scss'
+})
+export class AproposComponent {
+
+}
