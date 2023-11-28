9c39cf832e29e98ed4b40885199636ff0e6fe3e9
baptiste.nebout <baptiste16300@gmail.com>
2023-11-28 23:31:22 +0100

    ajout du carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatTabsModule} from '@angular/material/tabs';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { ContactComponent } from './contact/contact.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTabsModule, CatalogueComponent, ContactComponent, AproposComponent, SlickCarouselModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'CUBE3_BREIZHSPORT_FRONT';
+  slides = [
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/333333"},
+    {img: "http://placehold.it/350x150/666666"}
+  ];
+  slideConfig = {
+    "slidesToShow": 1,
+    "slideToScroll": 1,
+    "autoplay": true,
+    "autoplaySpeed": 5000,
+    "pauseOnHover": true,
+    "infinite": true,
+  }
 }
