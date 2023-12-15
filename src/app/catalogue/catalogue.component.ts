c9d1704137f97b49ac70903c411e857f4c6de038
baptiste.nebout <baptiste16300@gmail.com>
2023-12-15 15:57:25 +0100

    création de la page catalogue

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//import des elements de material
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+
+//import des mocks
+import { articlesList } from '../mock-articles';
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatTabsModule, MatButtonModule, MatChipsModule],
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.scss'
 })
-export class CatalogueComponent {
+export class CatalogueComponent implements OnInit {
+  articlesList = articlesList
+  ngOnInit(): void {
+    console.log(articlesList);
+  }
 
 }
