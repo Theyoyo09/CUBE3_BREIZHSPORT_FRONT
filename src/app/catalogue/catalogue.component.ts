050728b5c7016c3039d3fd3aff3a27aa8bc28d09
baptiste.nebout <baptiste16300@gmail.com>
2023-12-20 09:05:29 +0100

    ajout de la pagination de catalogue WIP

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 //import des elements de material
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatButtonModule} from '@angular/material/button';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 
 //import des mocks
 import { articlesList } from '../mock-articles';
@@ -12,14 +14,18 @@ import { articlesList } from '../mock-articles';
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatTabsModule, MatButtonModule, MatChipsModule],
+  imports: [CommonModule, MatCardModule, MatTabsModule, MatButtonModule, MatChipsModule, MatPaginatorModule],
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.scss'
 })
 export class CatalogueComponent implements OnInit {
   articlesList = articlesList
+  totalArticle = articlesList.length
   ngOnInit(): void {
     console.log(articlesList);
   }
-
+}
+export class PaginatorConfigurableExample {
+  pageSize = 10;
+  pageSizeOptions = [5, 10, 25, 100];
 }
