106b4b9b9166eecc9aa4044b621bac69c5575d2e
baptiste.nebout <baptiste16300@gmail.com>
2023-12-28 18:46:28 +0100

    modification de l'emplacement de la page home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,47 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-//import des elements de ngx-bootstrap
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-
 //import des elements de material
-import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 
 //import la config de la liste d'objet
 import { ConfigComponent } from "./config/config.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 //import les component 
+import { HomeComponent } from "./home/home.component";
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { ContactComponent } from './contact/contact.component';
 import { AproposComponent } from './apropos/apropos.component';
+import { PanierComponent } from './panier/panier.component';
+// import { login } from './login/login.component';
 
-//import des mocks
-import { articlesList } from './mock-articles';
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatTabsModule, CatalogueComponent, ContactComponent, AproposComponent, SlickCarouselModule, MatCardModule, ConfigComponent, MatButtonModule, MatIconModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, MatTabsModule, CatalogueComponent, ContactComponent, AproposComponent, ConfigComponent, PanierComponent, HomeComponent, MatIconModule, MatCardModule, MatButtonModule]
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'CUBE3_BREIZHSPORT_FRONT';
-  articlesList = articlesList
-  slideConfig = {
-    "slidesToShow": 1,
-    "slideToScroll": 1,
-    "autoplay": true,
-    "autoplaySpeed": 500000,
-    "pauseOnHover": true,
-    "infinite": true,
-  } 
   constructor() {}
-  ngOnInit(): void {
-    console.log(articlesList);
-  }
+  
 }
