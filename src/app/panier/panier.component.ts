3259d763ea73e01206f88b5d04b70a9f8fade1b9
baptiste.nebout <baptiste16300@gmail.com>
2024-01-14 21:51:35 +0100

    création du panier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//import des elements de material
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatPaginatorModule} from '@angular/material/paginator';
+//import des mocks
+import { articlesList } from '../models/mock-articles';
 
 @Component({
   selector: 'app-panier',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatTabsModule, MatButtonModule, MatChipsModule, MatPaginatorModule],
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.scss'
 })
-export class PanierComponent {
-
+export class PanierComponent implements OnInit {
+  prix:number = 10.20;
+  articlesList = articlesList
+  totalArticle = articlesList.length
+  ngOnInit(): void {
+    console.log(articlesList);
+  }
 }
