819dc29dc3e50b4c8b925a8506e208f990791068
baptiste.nebout <baptiste16300@gmail.com>
2024-01-14 21:50:37 +0100

    suppression des tabs et ajout du header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-//import des elements de material
-import {MatTabsModule} from '@angular/material/tabs';
-
 //import la config de la liste d'objet
 import { ConfigComponent } from "./config/config.component";
 import { MatIconModule } from '@angular/material/icon';
@@ -13,9 +10,6 @@ import {MatButtonModule} from '@angular/material/button';
 
 //import les component 
 import { HomeComponent } from "./home/home.component";
-import { CatalogueComponent } from './catalogue/catalogue.component';
-import { ContactComponent } from './contact/contact.component';
-import { AproposComponent } from './apropos/apropos.component';
 import { PanierComponent } from './panier/panier.component';
 // import { login } from './login/login.component';
 
@@ -26,7 +20,7 @@ import { PanierComponent } from './panier/panier.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, MatTabsModule, CatalogueComponent, ContactComponent, AproposComponent, ConfigComponent, PanierComponent, HomeComponent, MatIconModule, MatCardModule, MatButtonModule]
+    imports: [CommonModule, RouterOutlet, ConfigComponent, PanierComponent, HomeComponent, MatIconModule, MatCardModule, MatButtonModule]
 })
 export class AppComponent {
   title = 'CUBE3_BREIZHSPORT_FRONT';
