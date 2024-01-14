1dccc600775344d8c10c78290e4a7df85af91c55
baptiste.nebout <baptiste16300@gmail.com>
2024-01-14 21:49:53 +0100

    formatage et ajout des tabs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,14 +7,22 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 //import des elements de material
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
 
 //import des mocks
 import { articlesList } from '../models/mock-articles';
 
+//import les component 
+import { CatalogueComponent } from '../catalogue/catalogue.component';
+import { ContactComponent } from '../contact/contact.component';
+import { AproposComponent } from '../apropos/apropos.component';
+import { PanierComponent } from '../panier/panier.component';
+// import { login } from './login/login.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SlickCarouselModule, MatButtonModule, MatCardModule, CommonModule],
+  imports: [SlickCarouselModule, MatButtonModule, MatCardModule, CommonModule, MatTabsModule, CatalogueComponent, ContactComponent, AproposComponent, PanierComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
