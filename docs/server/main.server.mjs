6e58939c48e49d61b5ff31db62be6935a504d540
Theyoyo09 <90200931+Theyoyo09@users.noreply.github.com>
2024-02-28 19:02:51 +0100

    redeploy fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XGD7SOTI.mjs";import"./chunk-QONJGTEQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GGQU5ATQ.mjs";import"./chunk-QONJGTEQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
