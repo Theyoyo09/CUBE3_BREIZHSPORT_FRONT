43487017d9b74a3002144607b835b4b08b6a8ef7
baptiste.nebout <baptiste16300@gmail.com>
2024-03-06 18:36:08 +0100

    model_commandes ne sert plus à rien

--- src/app/models/model_commandes.ts
+++
@@ -1,6 +0,0 @@
-import * as mongoose from 'mongoose';
-import { Commande } from "./model_commande";
-
-const commandeSchema = new mongoose.Schema<Commande>;
-
-export const CommandeModel = mongoose.model('Commande', commandeSchema);
