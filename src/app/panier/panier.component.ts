4afd91893bc63d1fc68ff4cbf6a591e1dadcfbb0
baptiste.nebout <baptiste16300@gmail.com>
2024-03-06 18:37:48 +0100

    ajout du nouveau formulaire et envoie à l'endpoint de l'ajout de commande

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -7,19 +7,56 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatPaginatorModule} from '@angular/material/paginator';
 //import des mocks
+import { CommandeService } from "../services/commande.service";
+import { Commande } from "../models/model_commande";
 import { articlesList } from '../mock/mock-articles';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-panier',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatTabsModule, MatButtonModule, MatChipsModule, MatPaginatorModule],
+  imports: [CommonModule, MatCardModule, MatTabsModule, MatButtonModule, MatChipsModule, MatPaginatorModule, FormsModule, MatInputModule],
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.scss'
 })
 export class PanierComponent implements OnInit {
+  constructor(private commandeService: CommandeService) {}
   prix:number = 10.20;
   articlesList = articlesList
   totalArticle = articlesList.length
+
+  displayCommande() {
+    let formEmailTel = document.getElementById("valideCommande");
+    if(formEmailTel){
+      formEmailTel.style.display="flex";
+    };
+  };
+
+  valideCommande(commandeForm: NgForm) {
+    if (commandeForm.invalid) {
+      return;
+    }
+
+    const formData = commandeForm.value;
+
+    const commande: Commande = {
+      idUser: '1',
+      phoneNumber: formData.phoneNumber,
+      email: formData.email,
+      listArticles: [],
+      statusCommande: false,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+
+    this.commandeService.addCommande(commande).subscribe({
+      next(value){ return console.log('Commande ajoutée avec succès.' + value); },
+      error(err){ return console.error('Erreur lors de l\'ajout de la commande :', err) },
+      complete(){ return console.log('commande ajoutée go back to home'); }
+    });
+  }
+
   ngOnInit(): void {
     //console.log(articlesList);
   }
--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -50,6 +50,6 @@ export class ProfilComponent implements OnInit {
     .subscribe((data)=>{
       this.commandes = data;
       console.log("les commandes", this.commandes);
-    })
+    });
   }
 }
