2434b05993467cc4c02c318adab9c7058192387b
Theyoyo09 <90200931+Theyoyo09@users.noreply.github.com>
2024-03-10 22:12:36 +0100

    merci baptiste VTF

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MW5NY6FS.mjs";import"./chunk-UUOXFFQ2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BKGZZDT2.mjs";import"./chunk-UUOXFFQ2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
